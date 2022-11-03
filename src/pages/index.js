5113e743c3fb8f1399d6acc1b272cdc19f4b24c9
elijahcw-git <85442180+elijahcw-git@users.noreply.github.com>
2022-11-02 19:54:26 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+    AboutSection,
+    ContactSection,
+    HeroSection,
+    InterestsSection,
+    Page,
+    ProjectsSection,
+    Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+    return (
+        <>
+            <Seo title="Elijah W. - Dallas, TX" />
+            <Page useSplashScreenAnimation>
+                <HeroSection sectionId="hero" />
+                {/* <ArticlesSection
+                    sectionId="articles"
+                    heading="Latest Articles"
+                    sources={["Medium"]}
+                /> */}
+                <AboutSection sectionId="about" heading="A Quick Bio!" />
+                <InterestsSection
+                    sectionId="details"
+                    heading="My Developer Skills and Certifications"
+                />
+                <ProjectsSection
+                    sectionId="features"
+                    heading="Projects I'm Proud Of"
+                />
+                <ContactSection sectionId="github" heading="Contact Me" />
+            </Page>
+        </>
+    );
+}
