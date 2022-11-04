305c383d3d68221a38ab3bf87551d095dd11bd1d
elijahcw-git <85442180+elijahcw-git@users.noreply.github.com>
2022-11-04 08:43:12 -0500

    troubleshooting

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,15 +17,15 @@ module.exports = {
                 },
 
                 contentDirectory: "./content",
-                // blogSettings: {
-                //     path: "/blog", // Defines the slug for the blog listing page
-                //     usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-                // },
-                // googleAnalytics: {
-                //     trackingId: "UA-XXXXXX-X",
-                //     anonymize: true, // Default true
-                //     environments: ["production", "development"] // Default ["production"]
-                // }
+                blogSettings: {
+                    path: "/blog", // Defines the slug for the blog listing page
+                    usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+                },
+                googleAnalytics: {
+                    trackingId: "UA-XXXXXX-X",
+                    anonymize: true, // Default true
+                    environments: ["production", "development"], // Default ["production"]
+                },
             },
         },
     ],
